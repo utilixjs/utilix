d4d35152cbbbe9be04c5dfd81553b9f7be157490
Waleed Khaled <waleed1kh@outlook.com>
2023-05-09 17:19:50 +0400

    feat(isArray): new function isArrayLike

--- packages/utilix/src/basics/isArray/index.test.ts
+++ packages/utilix/src/basics/isArray/index.test.ts
@@ -1,5 +1,7 @@
 import { describe, it, expect } from 'vitest';
-import { isArray } from './';
+import { isArray, isArrayLike } from './';
+
+const args = (function (..._: any[]) { return arguments; }).apply(null, [1, 2]);
 
 describe('isArray', () => {
 	it('should return `true` for arrays', () => {
@@ -13,10 +15,30 @@ describe('isArray', () => {
 		expect(isArray(null)).toBeFalsy();
 		expect(isArray(true)).toBeFalsy();
 		expect(isArray({ '0': 1, 'length': 1 })).toBeFalsy();
-		expect(isArray(((function (..._: any[]) { return arguments; }).apply(null, [1, 2])))).toBeFalsy();
+		expect(isArray(args)).toBeFalsy();
 		expect(isArray(new Set())).toBeFalsy();
 		expect(isArray(new Date())).toBeFalsy();
 		expect(isArray(new Error())).toBeFalsy();
 		expect(isArray(/x/)).toBeFalsy();
+	});
+});
+
+describe('isArrayLike', () => {
+	it('should return `true` for array-like values', () => {
+		expect(isArrayLike([])).toBeTruthy();
+		expect(isArrayLike([1, 2, 3])).toBeTruthy();
+		expect(isArrayLike(args)).toBeTruthy();
+		expect(isArrayLike({ '0': 1, 'length': 1 })).toBeTruthy();
+		expect(isArrayLike('a')).toBeTruthy();
+	});
+
+	it('should return `false` for non-array-like values', () => {
+		expect(isArrayLike(1)).toBeFalsy();
+		expect(isArrayLike(null)).toBeFalsy();
+		expect(isArrayLike(true)).toBeFalsy();
+		expect(isArrayLike(new Set())).toBeFalsy();
+		expect(isArrayLike(new Date())).toBeFalsy();
+		expect(isArrayLike(new Error())).toBeFalsy();
+		expect(isArrayLike(/x/)).toBeFalsy();
 	});
 });
--- packages/utilix/src/basics/isArray/index.ts
+++ packages/utilix/src/basics/isArray/index.ts
@@ -1,1 +1,10 @@
 export const isArray = Array.isArray;
+
+function isLength(value?: any) {
+	return typeof value === 'number' && value > -1 &&
+		value % 1 === 0 && value <= Number.MAX_SAFE_INTEGER;
+}
+
+export function isArrayLike(value?: any): value is ArrayLike<any> {
+	return value != null && typeof value !== 'function' && isLength(value.length);
+}
