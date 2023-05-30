5b286ca6479dcfdabd0a4fe529ac63e333726e11
Waleed Khaled <waleed1kh@outlook.com>
2023-05-30 19:48:07 +0400

    chore: lint config update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,41 +1,42 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
 	root: true,
-	parser: "@typescript-eslint/parser",
-	plugins: ["@typescript-eslint"],
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
 	extends: [
-		"eslint:recommended",
-		"plugin:@typescript-eslint/eslint-recommended",
-		"plugin:@typescript-eslint/recommended"
+		'eslint:recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended'
 	],
 	env: {
 		es2021: true,
 		node: true
 	},
 	rules: {
-		"indent": ["error", "tab"],
-		"semi": ["error", "always"],
+		'indent': ['error', 'tab', { 'SwitchCase': 1 }],
+		'semi': ['error', 'always'],
+		'prefer-const': 'warn',
 
-		"@typescript-eslint/array-type": "warn",
-		"@typescript-eslint/ban-ts-comment": ["warn", {
-			"ts-expect-error": "allow-with-description",
-			"ts-ignore": "allow-with-description",
-			"ts-nocheck": true,
-			"ts-check": false,
+		'@typescript-eslint/array-type': 'warn',
+		'@typescript-eslint/ban-ts-comment': ['warn', {
+			'ts-expect-error': 'allow-with-description',
+			'ts-ignore': 'allow-with-description',
+			'ts-nocheck': true,
+			'ts-check': false,
 			minimumDescriptionLength: 3
 		}],
-		"@typescript-eslint/ban-types": ["error", {
+		'@typescript-eslint/ban-types': ['error', {
 			types: {
-				"Function": false
+				'Function': false
 			},
 		}],
-		"@typescript-eslint/no-empty-function": "off",
-		"@typescript-eslint/no-explicit-any": "off",
-		"@typescript-eslint/no-for-in-array": "error",
-		"@typescript-eslint/no-unused-vars": ["warn", {
-			varsIgnorePattern: "^_",
-			argsIgnorePattern: "^_",
-			caughtErrorsIgnorePattern: "^_"
+		'@typescript-eslint/no-empty-function': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-for-in-array': 'error',
+		'@typescript-eslint/no-unused-vars': ['warn', {
+			varsIgnorePattern: '^_',
+			argsIgnorePattern: '^_',
+			caughtErrorsIgnorePattern: '^_'
 		}]
 	}
 };
