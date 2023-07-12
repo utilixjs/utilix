6871e5d0ef3b551900f06117d9c523e4def689c1
Waleed Khaled <waleed1kh@outlook.com>
2023-07-12 14:06:42 +0400

    chore(isNumeric): function update

--- packages/utilix/src/basics/isNumber/index.ts
+++ packages/utilix/src/basics/isNumber/index.ts
@@ -7,13 +7,13 @@ export function isNumberObject(value?: unknown): value is number | Number {
 	return isNumber(value) || (value instanceof Number);
 }
 
-export function isNumeric(value: unknown) {
+export function isNumeric(value?: unknown): value is number | string {
 	switch (typeof value) {
 		case 'number':
 			return value - value === 0;
 		case 'string':
 			if (value.trim() !== '')
-				return Number.isFinite ? Number.isFinite(+value) : isFinite(+value);
+				return Number.isFinite(+value);
 			return false;
 		default:
 			return false;
