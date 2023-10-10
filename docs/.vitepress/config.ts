8d66e140279b9e0065be1ab46aaffebae0f5aac7
Waleed Khaled <waleed1kh@outlook.com>
2023-10-10 19:19:46 +0400

    docs: open graph metadata

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,15 +1,26 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from 'vitepress';
+
+const title = "Utilix";
+const description = "Modern and flexible utilities library for JavaScript";
+const ogImg = 'https://utilix.dev/media/og.png';
 
 export default defineConfig({
 	lang: 'en-US',
-	title: "Utilix",
-	description: "A modern and flexible utilities library for JavaScript",
+	title,
+	description,
 
 	head: [
 		['link', { rel: 'icon', type: 'image/png', href: '/media/logo/u4t1x1-256.png' }],
 		['meta', { name: 'theme-color', content: '#ce996a' }],
+		['meta', { property: 'og:title', content: title }],
+		['meta', { property: 'og:description', content: description }],
+		['meta', { property: 'og:image', content: ogImg }],
+		['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+		['meta', { name: 'twitter:creator', content: '@waleed1kh' }],
+		['meta', { name: 'twitter:image', content: ogImg }],
 	],
 
+	cleanUrls: true,
 	themeConfig: {
 		logo: { src: '/media/logo/u4ts.png' },
 
