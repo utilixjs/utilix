ffe46fd70c10388091026f56997a08599704b9b1
Waleed Khaled <waleed1kh@outlook.com>
2023-10-30 21:16:15 +0400

    feat(types): new type `Mutable`

--- packages/utilix/src/types.ts
+++ packages/utilix/src/types.ts
@@ -1,6 +1,10 @@
 export type PartiallyRequired<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K>>;
 export type PartiallyOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
+export type Mutable<Type> = {
+	-readonly [P in keyof Type]: Type[P];
+};
+
 export type Func<TResult = void, TArgs extends any[] = []> = (...args: TArgs) => TResult;
 export type Action<TArgs extends any[] = []> = Func<void, TArgs>;
 
