5f48614816176a0d5aeff311ece83e760da42417
Waleed Khaled <waleed1kh@outlook.com>
2023-12-11 21:59:23 +0400

    docs(time): add `Interval` documentation and TSDoc

---
+++ packages/utilix/src/time/Interval/index.md
@@ -0,0 +1,3 @@
+# Interval
+
+Repeatedly calls a function or executes a code snippet, with a time delay between each call, with controls to pause & resume the interval.
--- packages/utilix/src/time/Interval/index.ts
+++ packages/utilix/src/time/Interval/index.ts
@@ -1,19 +1,36 @@
 import type { Action } from "@/types";
 import { isNumber } from "@/basics/isNumber";
 
+/**
+ * Interface that represent an interval.
+ */
 export interface IInterval {
+	/** Delay in milliseconds. */
 	readonly delay?: number;
+	/** Whether the interval is currently active. */
 	readonly isActive: boolean;
 
+	/** Pauses the interval. */
 	pause(): void;
+	/** Resumes the interval. */
 	resume(): void;
 }
 
+/**
+ * Options for the Interval class.
+ */
 export interface IntervalOptions {
+	/** Delay in milliseconds. */
 	delay?: number;
+	/** Whether to start the interval immediately. */
 	immediate?: boolean;
 }
 
+/**
+ * Represents a repeated calls of a function with a time delay between each call.
+ *
+ * @template TArgs Type of arguments to pass to the callback function.
+ */
 export class Interval<TArgs extends any[] = []> implements IInterval {
 	private readonly _cb: Action<TArgs>;
 	private readonly _delay?: number;
@@ -22,9 +39,27 @@ export class Interval<TArgs extends any[] = []> implements IInterval {
 	private _intervalId: ReturnType<typeof setInterval> | null = null;
 	private _isActive = false;
 
+	/**
+	 * Creates a new Interval.
+	 *
+	 * @param callback The function to be called when the interval elapses.
+	 * @param delay The delay between each call in milliseconds.
+	 */
 	constructor(callback: Action, delay?: number);
+	/**
+	 * Creates a new Interval.
+	 *
+	 * @param callback The function to be called when the interval elapses.
+	 * @param options The options for the interval.
+	 */
 	constructor(callback: Action, options?: IntervalOptions);
-	constructor(callback: Action<TArgs>, delay?: number, ...args: TArgs);
+	/**
+	 * Creates a new Interval.
+	 *
+	 * @param callback The function to be called when the interval elapses.
+	 * @param options The options for the interval.
+	 * @param args Arguments to pass to the callback function.
+	 */
 	constructor(callback: Action<TArgs>, options?: number | IntervalOptions, ...args: TArgs);
 	constructor(callback: Action<TArgs>, options: number | IntervalOptions = {}, ...args: TArgs) {
 		this._args = args;
@@ -38,10 +73,12 @@ export class Interval<TArgs extends any[] = []> implements IInterval {
 			this.resume();
 	}
 
+	/** @inheritdoc */
 	get isActive() {
 		return this._isActive;
 	}
 
+	/** @inheritdoc */
 	get delay() {
 		return this._delay;
 	}
@@ -53,11 +90,13 @@ export class Interval<TArgs extends any[] = []> implements IInterval {
 		}
 	}
 
+	/** @inheritdoc */
 	pause() {
 		this._isActive = false;
 		this.clean();
 	}
 
+	/** @inheritdoc */
 	resume() {
 		this._isActive = true;
 		this.clean();
