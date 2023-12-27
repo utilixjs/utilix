c6683231a5e1128dbe96a1d69cd9fdab640b492b
Waleed Khaled <waleed1kh@outlook.com>
2023-12-27 22:13:37 +0400

    chore: update `isFunction` type

--- packages/utilix/src/basics/isFunction/index.ts
+++ packages/utilix/src/basics/isFunction/index.ts
@@ -4,6 +4,6 @@
  * @param value The value to check.
  * @returns `true` if value is a function, otherwise `false`.
  */
-export function isFunction(value: unknown): value is Function {
+export function isFunction(value: unknown): value is (...args: any[]) => any {
 	return typeof value === 'function';
 }
